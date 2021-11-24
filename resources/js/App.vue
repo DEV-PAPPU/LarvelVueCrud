f754369da3c2819cbf66597b3c8ff6abe1a9048d
dev pappu <pappusha629@gmail.com>
2021-11-24 15:17:34 +0600

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <router-view class="view one"></router-view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+
+    mounted(){
+        console.log('Main App Component ')
+        console.log('router', this.router)
+
+    }
+}
+</script>
